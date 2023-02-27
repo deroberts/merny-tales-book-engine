802af85afd04f987ecca75437b5534ee6096ee79
deroberts <s501656@gmail.com>
2023-02-27 12:24:07 -0600

    added comment to try heroku again.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
+  //make sure my key value is correct
   process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
   {
     useNewUrlParser: true,
